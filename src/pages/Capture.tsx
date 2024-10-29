b735d90521f29f64ce803331129a037f25706675
Andriamahay11master <hirimanana@yahoo.fr>
2024-10-29 15:48:28 +0300

    update page Capture

--- src/pages/Capture.tsx
+++ src/pages/Capture.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import CaptureButton from "../components/capture/CaptureButton";
+
 export default function Capture(){
+    const [capturedImage, setCapturedImage] = useState<string | null>(null);
+
+     // Fonction pour gérer l'image capturée
+    const handleImageCapture = (imageData: string) => {
+        setCapturedImage(imageData); // Stocke l'image pour affichage
+    };
+
     return (
-        <>
-            <p>Page Capture</p>
-        </>
+        <div className="main-page">
+            <div className="container">
+                <CaptureButton onCapture={handleImageCapture}/>
+                {capturedImage && (
+                    <div style={{ marginTop: '20px' }}>
+                    <h2>Preview</h2>
+                    <img 
+                        src={capturedImage} 
+                        alt="Captured preview" 
+                        style={{ maxWidth: '100%', height: 'auto', borderRadius: '8px', border: '1px solid #ddd' }} 
+                    />
+                    </div>
+                )}
+            </div>
+        </div>
     )
 }
